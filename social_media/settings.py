0cd523810edc780175fc349762e67f6441dc3d53
Oleksandr <sasha291108@gmail.com>
2025-10-07 22:02:22 +0200

    db feature

--- social_media/settings.py
+++ social_media/settings.py
@@ -80,13 +80,16 @@ WSGI_APPLICATION = 'social_media.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
 
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,  # тримає з’єднання відкритим
+        ssl_require=True,  # Render використовує SSL
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
